fc5a11fd69cbc1b077f1d381b6e0b776c6a01e3e
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-26 20:03:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,4 @@ def prime?(numbers)
   numbers.each do |number|
   Prime.prime?(number)
 end
-prime?()
-
-describe "prime?" do
-  it 'returns true for prime numbers' do
-    expect(prime?(2)(3)(11)(105557)(-1)(0)(1)(4)(40)(1763, 101013)).to be(false)
-  end
-end
+prime?([2, 3, 11, 105557, -1, 0, 1, 4, 40, 1763, 10101
