fa527ef5aadf409e5e9a77c4cdf9ec298be165f7
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-26 20:02:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,3 +6,8 @@ def prime?(numbers)
 end
 prime?()
 
+describe "prime?" do
+  it 'returns true for prime numbers' do
+    expect(prime?(2)(3)(11)(105557)(-1)(0)(1)(4)(40)(1763, 101013)).to be(false)
+  end
+end
