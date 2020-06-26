09fee51b74d9cc252fe64cf996987f9f74e27ed9
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-26 20:04:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,4 +4,4 @@ def prime?(numbers)
   numbers.each do |number|
   Prime.prime?(number)
 end
-prime?([2, 3, 11, 105557, -1, 0, 1, 4, 40, 1763, 10101
+prime?([2, 3, 11, 105557, -1, 0, 1, 4, 40, 1763, 101013])
